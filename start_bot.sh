84570a82c669098cf18ef7063f39dcef311011ef
quattro <808330+qorron@users.noreply.github.com>
2022-01-14 20:17:25 +0100

    removed outdated option

--- start_bot.sh
+++ start_bot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-perl bot2.pl --tg_key '123:abc' --use_mutex --city your_city 2>&1 | tee -a /tmp/beta_decoder_bot.log
+perl bot2.pl --tg_key '123:abc' --city your_city 2>&1 | tee -a /tmp/beta_decoder_bot.log
